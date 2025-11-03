043844ed59345a0f273e24cc5b801787166a26fa
devil666face <in@d6f.ru>
2025-11-03 22:00:43 +0300

    add: unsolth depedencies group

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+http://127.0.0.1:3000/ - open-webui
+
 ### Fix nvidia error for Ubuntu 22.04
 
 ```bash
--- ollama/entrypoint.sh
+++ ollama/entrypoint.sh
@@ -4,7 +4,7 @@ set -eE
 trap 'echo "Error: line: $LINENO: $(sed -n "${LINENO}p" "$0")"; exit 1' ERR
 
 if [ -z "$1" ]; then
-	jupyter lab --ip 0.0.0.0 --allow-root &
+	jupyter lab --ip 0.0.0.0 --allow-root --NotebookApp.token="$JUPITER_TOKEN" &
 	ollama serve
 else
 	exec "$@"
